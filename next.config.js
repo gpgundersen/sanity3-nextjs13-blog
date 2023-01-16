0fa48956d2e924ee0031dff8dceec18e997cbcd5
Grant Gundersen <gpgundersen@icloud.com>
2023-01-16 03:36:00 -0800

    committing first

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  _experimental: {
+    appDir: true
+  },
+  get experimental() {
+    return this._experimental;
+  },
+  set experimental(value) {
+    this._experimental = value;
+  },
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
+};
